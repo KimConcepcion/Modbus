da443b4bdcb9fe228ed14f85ff1fa88df483d43d
Kim Nielsen <kimnielsen33333@gmail.com>
2020-04-02 14:20:12 +0200

    Update modbus_slave.py

--- modbus_slave.py
+++ modbus_slave.py
@@ -7,33 +7,34 @@ from pymodbus.server.sync import ModbusTcpServer
 from pymodbus.framer.socket_framer import ModbusSocketFramer
 from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
-import logging
-
+from modbus_request_handler import ModbusRequestHandler
 
 # --------------------------------------------------------------------------- #
 # Modbus TCP Server/Slave Class
 #
-# TODO: Implement conversion of hr-register value to 16bit & split into bits
+# TODO: 
 # --------------------------------------------------------------------------- #
-class ModbusSlaveUnit():
+class ModbusServer():
 
     def __init__(self):
         self._slave_id   = 0x02
         self._start_addr = 0x00
-        self._end_addr   = 0x7c
+        self._end_addr   = 0x7d # 124
         self._data       = [0]
         self._init_modbus_slave()
 
     def _init_modbus_slave(self):
         self._slaves  = { self._slave_id : ModbusSlaveContext(di=None, co=None, ir=None, hr=ModbusSequentialDataBlock(self._start_addr, self._data*self._end_addr)) }
         self._context = ModbusServerContext( slaves=self._slaves, single=False )
+        
 
     def start_modbus_slave(self, host, port, custom_functions=[], **kwargs):
         print('Starting Modbus TCP server at IP: %s on PORT: %s' %(host, port))
         self._identity = None
         self._address  = (host, port)
         self._framer   = kwargs.pop("framer", ModbusSocketFramer)
-        self._server   = ModbusTcpServer(self._context, self._framer, self._identity, self._address, **kwargs)
+        self._handler  = ModbusRequestHandler
+        self._server   = ModbusTcpServer(self._context, self._framer, self._identity, self._address, self._handler, **kwargs)
 
         for f in custom_functions:
             self._server.decoder.register(f)
@@ -41,16 +42,6 @@ class ModbusSlaveUnit():
 
     def close_modbus_slave(self):
         self._server.server_close()
-
-    def _get_register_bitsself(self, data):
-        '''
-        Fetch individual bits of register by converting 
-        data into string based 16bit value.
-        Returns bits in a list format.
-        '''
-        bits = '{0:016b}'.format(data)
-        bits = list(bits)
-        return bits
 
 
 # --------------------------------------------------------------------------- #
@@ -61,5 +52,5 @@ if __name__ == "__main__":
     HOST = '192.168.127.7'
     PORT = 502
 
-    sp_1 = ModbusSlaveUnit()
+    sp_1 = ModbusServer()
     sp_1.start_modbus_slave(HOST, PORT)
